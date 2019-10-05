9ed121598a774bcfd8aeee6f685bc630e495502e
Luan Ricardo De Luna Vieira Venancio <luan_ricardo2@hotmail.com>
2019-10-05 02:32:16 -0300

    First Commit?

---
+++ src/main/java/br/com/nova/roma/tap/projeto/App.java
@@ -0,0 +1,13 @@
+package br.com.nova.roma.tap.projeto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
